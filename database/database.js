a4b1ee74496b4b5cbdf2d04a816d42934a7a022c
Agnes <faustillonas@gmail.com>
2025-08-28 23:22:59 +0700

    Update .gitignore and database config

--- database/database.js
+++ database/database.js
@@ -1,15 +1,16 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../marketin-3a1b3-firebase-adminsdk-fbsvc-cfaef28dc2.json");
+const firebaseConfig = {
+  apiKey: "AIzaSyChaoS6Ep5C9fph8_Dohs-XsHo6r6cwIFc",
+  authDomain: "marketin-3a1b3.firebaseapp.com",
+  projectId: "marketin-3a1b3",
+  messagingSenderId: "385478346259",
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+admin.initializeApp(firebaseConfig);
 
-// Firestore
 const db = admin.firestore();
 
-// Authentication
 const auth = admin.auth();
 
 module.exports = { admin, db, auth };
