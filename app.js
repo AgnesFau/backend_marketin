c0371b690dca577d973293ecb6c69f8c9cdeb135
Agnes <faustillonas@gmail.com>
2025-08-28 23:53:47 +0700

    feat: login

--- app.js
+++ app.js
@@ -1,55 +1,90 @@
-var express = require('express');
-var swaggerDoc = require('swagger-jsdoc');
-var swaggerUI = require('swagger-ui-express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var swaggerDoc = require("swagger-jsdoc");
+var swaggerUI = require("swagger-ui-express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 var testsRouter = require("./routes/tests");
 
 var app = express();
 
 var options = {
-    swaggerDefinition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Yareu API",
-            version: "1.0.0",
-            description: "API documentation for Yareu"
-        }
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Yareu API",
+      version: "1.0.0",
+      description: "API documentation for Yareu",
     },
-    apis: ["app.js"]
+  },
+  apis: ["app.js"],
 };
 
 var swagger = swaggerDoc(options);
-app.use('/swaggerdocs', swaggerUI.serve, swaggerUI.setup(swagger));
+app.use("/swaggerdocs", swaggerUI.serve, swaggerUI.setup(swagger));
 
 /**
- * @openapi 
+ * @openapi
  * /tests/hello:
  *   get:
  *     description: "Returns a hello world message"
  *     responses:
  *       200:
  *         description: "A hello world message"
- * /users/getallusers:
- *   get:
- *     description: "Returns a list of all users"
+ * /users/login:
+ *   post:
+ *     summary: Login user dengan email & password (Firebase Authentication)
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: testuser@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: testuser123
  *     responses:
  *       200:
- *         description: "A list of users"
+ *         description: Login sukses, return token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 idToken:
+ *                   type: string
+ *                 refreshToken:
+ *                   type: string
+ *                 expiresIn:
+ *                   type: string
+ *                 localId:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       401:
+ *         description: Email atau password salah
  */
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tests', testsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/tests", testsRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,30 @@
 var express = require("express");
 const { auth } = require("../database/database");
+const axios = require("axios");
 var router = express.Router();
 
-/* GET all users */
-router.get("/getallusers", async function (req, res, next) {
+/* POST login */
+router.post("/login", async function (req, res) {
   try {
-    const listUsers = await auth.listUsers(1000);
-    const users = listUsers.users.map((user) => ({
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      phoneNumber: user.phoneNumber,
-      disabled: user.disabled,
-      metadata: user.metadata,
-    }));
+    const { email, password } = req.body;
 
-    res.status(200).json(users);
+    if (!email || !password) {
+      return res.status(400).json({ error: "Missing email or password" });
+    }
+
+    const response = await axios.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyChaoS6Ep5C9fph8_Dohs-XsHo6r6cwIFc`,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      }
+    );
+
+    res.status(200).json(response.data);
   } catch (err) {
-    console.error("Error fetching users:", err);
-    res.status(500).json({ error: err.message });
+    console.error("Login error:", err.response?.data || err.message);
+    res.status(401).json({ error: "Invalid email or password" + err.message });
   }
 });
 
